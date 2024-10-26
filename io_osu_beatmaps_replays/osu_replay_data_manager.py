46ea962c5bc20a2925c709ab54ec696fe227dc6d
wavezz1 <henke.marc@gmx.de>
2024-10-26 16:30:39 +0200

    datamanager audio

--- io_osu_beatmaps_replays/osu_replay_data_manager.py
+++ io_osu_beatmaps_replays/osu_replay_data_manager.py
@@ -107,6 +107,10 @@ class OsuReplayDataManager:
             pitch = 0.75
         speaker.data.pitch = pitch
 
+        #FORCE UPDATE
+        speaker.data.muted = True
+        bpy.context.view_layer.update()
+        speaker.data.muted = False
         bpy.context.view_layer.update()
 
         print(f"Audio-Datei '{audio_filename}' erfolgreich importiert und mit {pitch}x Pitch dem Speaker hinzugefügt.")

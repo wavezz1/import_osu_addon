59b1f46406ebf1584ab5d1241835bfcd8b5f9d52
wavezz1 <henke.marc@gmx.de>
2024-11-22 21:52:08 +0100

    ui

--- osu_importer/utils/utils.py
+++ osu_importer/utils/utils.py
@@ -183,4 +183,4 @@ def flip_objects(prefixes, axis, invert_location=True, invert_scale=True):
 def update_override_mods(self, context):
     for prop_name in dir(self):
         if prop_name.startswith("override_") and prop_name != "override_mods":
-            setattr(self, prop_name, self.override_mods)
+            setattr(self, prop_name, False)

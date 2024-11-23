4b65fc14ffb37e9177a96b15fc52844c8abd74f9
wavezz1 <henke.marc@gmx.de>
2024-11-23 19:18:00 +0100

    combo circle

--- osu_importer/parsers/hitobjects.py
+++ osu_importer/parsers/hitobjects.py
@@ -14,8 +14,23 @@ class HitObject:
         self.duration_frames = None
         self.was_hit = False
         self.was_completed = False
+        self.combo_number = 0
+        self.is_new_combo = False
+        self.combo_color = None
 
 class HitObjectsProcessor:
+    COMBO_COLORS = [
+        "Blue",
+        "Red",
+        "Green",
+        "Yellow",
+        "Purple",
+        "Orange",
+        "Pink",
+        "Brown",
+        "Grey",
+        "Cyan",
+    ]
     def __init__(self, data_manager):
         self.data_manager = data_manager
         self.circles = []
@@ -25,6 +40,8 @@ class HitObjectsProcessor:
 
     def process_hitobjects(self):
         hitobjects = self.data_manager.osu_parser.hitobjects
+        current_combo = 0
+        combo_color_index = 0
 
         for line in hitobjects:
             parts = line.split(',')
@@ -35,6 +52,22 @@ class HitObjectsProcessor:
             extras = parts[5:]
 
             hit_object = HitObject(x, y, time, hit_type, hit_sound, extras)
+
+            # Prüfen auf New Combo-Flag
+            NEW_COMBO_FLAG = 4  # Bit 2 (0-indexed) repräsentiert New Combo in osu! Standard
+            is_new_combo = hit_type & NEW_COMBO_FLAG
+            hit_object.is_new_combo = bool(is_new_combo)
+
+            if is_new_combo:
+                current_combo = 1
+                combo_color_index = (combo_color_index + 1) % len(self.COMBO_COLORS)
+            else:
+                current_combo += 1
+
+            hit_object.combo_number = current_combo
+            hit_object.combo_color = self.COMBO_COLORS[combo_color_index]
+
+            # Einteilen in Typen
             if hit_type & 1:  # Circle
                 self.circles.append(hit_object)
             elif hit_type & 2:  # Slider

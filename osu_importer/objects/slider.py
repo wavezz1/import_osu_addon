ec7cedba37c3221597e372656eab859c4b2473fe
wavezz1 <henke.marc@gmx.de>
2024-11-29 11:32:38 +0100

    reverse

--- osu_importer/objects/slider.py
+++ osu_importer/objects/slider.py
@@ -181,9 +181,6 @@ class SliderCreator:
                     "reverse_tail": 'BOOLEAN',
                 }
 
-                fixed_values['reverse_head'] = False
-                fixed_values['reverse_tail'] = False
-
                 if self.hitobject.combo_number is not None:
                     fixed_values['combo'] = self.hitobject.combo_number
                     fixed_values['combo_color'] = self.hitobject.combo_color
